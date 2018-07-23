b37ff01e344372d51db2d8980005672842da93a9
Ahmed AbouZaid <ahmed.abouzaid@crossengage.io>
2018-07-23 11:22:07 +0200

    move pre-provision section to a separated function

--- proxmox/resource_vm_qemu.go
+++ proxmox/resource_vm_qemu.go
@@ -360,51 +360,9 @@ func resourceVmQemuCreate(d *schema.ResourceData, meta interface{}) error {
 		return err
 	}
 
-	preProvision := d.Get("preprovision").(bool)
-	if preProvision {
-		log.Print("[DEBUG] setting up SSH forward")
-		sshPort, err := pxapi.SshForwardUsernet(vmr, client)
-		if err != nil {
-			pmParallelEnd(pconf)
-			return err
-		}
+	// Apply pre-provision if enabled.
+	preprovision(d, pconf, client, vmr, true)
 
-		// Done with proxmox API, end parallel and do the SSH things
-		pmParallelEnd(pconf)
-
-		d.SetConnInfo(map[string]string{
-			"type":        "ssh",
-			"host":        d.Get("ssh_forward_ip").(string),
-			"port":        sshPort,
-			"user":        d.Get("ssh_user").(string),
-			"private_key": d.Get("ssh_private_key").(string),
-			"pm_api_url":  client.ApiUrl,
-			"pm_user":     client.Username,
-			"pm_password": client.Password,
-		})
-
-		switch d.Get("os_type").(string) {
-
-		case "ubuntu":
-			// give sometime to bootup
-			time.Sleep(9 * time.Second)
-			err = preProvisionUbuntu(d)
-			if err != nil {
-				return err
-			}
-
-		case "centos":
-			// give sometime to bootup
-			time.Sleep(9 * time.Second)
-			err = preProvisionCentos(d)
-			if err != nil {
-				return err
-			}
-
-		default:
-			return fmt.Errorf("Unknown os_type: %s", d.Get("os_type").(string))
-		}
-	}
 	return nil
 }
 
@@ -467,23 +425,8 @@ func resourceVmQemuUpdate(d *schema.ResourceData, meta interface{}) error {
 		return err
 	}
 
-	preProvision := d.Get("preprovision").(bool)
-	if preProvision {
-		sshPort, err := pxapi.SshForwardUsernet(vmr, client)
-		if err != nil {
-			pmParallelEnd(pconf)
-			return err
-		}
-		d.SetConnInfo(map[string]string{
-			"type":        "ssh",
-			"host":        d.Get("ssh_forward_ip").(string),
-			"port":        sshPort,
-			"user":        d.Get("ssh_user").(string),
-			"private_key": d.Get("ssh_private_key").(string),
-		})
-
-		pmParallelEnd(pconf)
-	}
+	// Apply pre-provision if enabled.
+	preprovision(d, pconf, client, vmr, false)
 
 	// give sometime to bootup
 	time.Sleep(9 * time.Second)
@@ -647,4 +590,62 @@ func updateDevicesDefaults(
 		}
 	}
 	return activeDevicesMap
+}
+
+func preprovision(
+	d *schema.ResourceData,
+	pconf *providerConfiguration,
+	client *pxapi.Client,
+	vmr *pxapi.VmRef,
+	systemPreProvision bool,
+) error {
+
+	if d.Get("preprovision").(bool) {
+		log.Print("[DEBUG] setting up SSH forward")
+		sshPort, err := pxapi.SshForwardUsernet(vmr, client)
+		if err != nil {
+			pmParallelEnd(pconf)
+			return err
+		}
+
+		// Done with proxmox API, end parallel and do the SSH things
+		pmParallelEnd(pconf)
+
+		d.SetConnInfo(map[string]string{
+			"type":        "ssh",
+			"host":        d.Get("ssh_forward_ip").(string),
+			"port":        sshPort,
+			"user":        d.Get("ssh_user").(string),
+			"private_key": d.Get("ssh_private_key").(string),
+			"pm_api_url":  client.ApiUrl,
+			"pm_user":     client.Username,
+			"pm_password": client.Password,
+		})
+
+		if systemPreProvision {
+			switch d.Get("os_type").(string) {
+
+			case "ubuntu":
+				// give sometime to bootup
+				time.Sleep(9 * time.Second)
+				err = preProvisionUbuntu(d)
+				if err != nil {
+					return err
+				}
+
+			case "centos":
+				// give sometime to bootup
+				time.Sleep(9 * time.Second)
+				err = preProvisionCentos(d)
+				if err != nil {
+					return err
+				}
+
+			default:
+				return fmt.Errorf("Unknown os_type: %s", d.Get("os_type").(string))
+			}
+		}
+	}
+
+	return nil
 }
